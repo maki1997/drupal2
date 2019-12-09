64bddf3c547ff74c63b6e5abb51222cfdbd6e8ee
maki1997 <makibijelic3377@gmail.com>
2019-12-09 09:46:38 +0100

    getting from xml moved to service

--- modules/custom/books/src/Controller/BooksController.php
+++ modules/custom/books/src/Controller/BooksController.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\books\Controller;
 
+use Drupal\books\Services\BooksService;
 use \Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Entity\Query\QueryInterface;
 use GuzzleHttp\Client;
@@ -11,63 +12,20 @@ use Drupal\Core\Entity\Query\Sql\QueryFactory;
 class BooksController extends  ControllerBase
 {
   protected $httpClient;
-  public function __construct(Client $httpClient){
+  protected $bService;
+  public function __construct(Client $httpClient,BooksService $booksService){
     $this->httpClient = $httpClient;
+    $this->bService = $booksService;
    }
 
   public static function create(ContainerInterface $container) {
     return new static(
       //$container->get('entity_type.manager'),
-      $container->get('http_client')
+      $container->get('http_client'),
+      $bService = $container->get('books.books_service')
     );
   }
 
-  public function getValuesFromExternalUri(){
-    $uri = "http://www.chilkatsoft.com/xml-samples/bookstore.xml";
-    try {
-      $response = $this->httpClient()->get($uri, array('headers' => array('Accept' => 'text/xml')));
-      $data = (string) $response->getBody();
-      if (empty($data)) {
-        return FALSE;
-      }
-    }
-    catch (RequestException $e) {
-      return FALSE;
-    }
-    $xmlToParse = simplexml_load_string($data);
-    $xml = json_decode(json_encode($xmlToParse),true);
-    return $xml;
-
-}
-
-  private function createContentTypesFromXml($xml){
-    foreach($xml as $books){
-      foreach($books as $book){
-        $title = $book['title'];
-        $price = $book['price'];
-        $isbn = $book['@attributes']['ISBN'];
-        $comments = $book['comments']['userComment'];
-        $comms = [];
-        if(is_array($comments)){
-          foreach($comments as $comment){
-            $comm = $comment;
-            $comms[] = $comm;
-          }
-        }else{
-          $comm = $comments;
-          $comms[] = $comm;
-        }
-        $newBook = array();
-        $newBook['type'] = 'book';
-        $newBook['title'] = $title;
-        $newBook['field_price'] = $price;
-        $newBook['field_isbn'] = $isbn;
-        $newBook['field_comments'] = $comms;
-        $bookToCreate = $this->entityTypeManager()->getStorage('node')->create($newBook);
-        $bookToCreate->save();
-      }
-    }
-  }
 
   public function getBooks(){
     $ids = \Drupal::entityQuery('node')
@@ -98,8 +56,8 @@ class BooksController extends  ControllerBase
     return $comments;
   }
   public function books(){
-    //$xml = $this->getValuesFromExternalUri();
-    //$this->createContentTypesFromXml($xml);
+    $xml = $this->bService->getValuesFromExternalUri();
+    $this->bService->createContentTypesFromXml($xml);
     return array(
       'books' => [
         '#theme' => 'books',
---
+++ modules/custom/books/src/Services/BooksService.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Drupal\books\Services;
+
+
+use GuzzleHttp\Client;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class BooksService
+{
+
+
+  public function getValuesFromExternalUri(){
+    $uri = "http://www.chilkatsoft.com/xml-samples/bookstore.xml";
+    try {
+      $response = \Drupal::httpClient()->get($uri, array('headers' => array('Accept' => 'text/xml')));
+      $data = (string) $response->getBody();
+      if (empty($data)) {
+        return FALSE;
+      }
+    }
+    catch (RequestException $e) {
+      return FALSE;
+    }
+    $xmlToParse = simplexml_load_string($data);
+    $xml = json_decode(json_encode($xmlToParse),true);
+    return $xml;
+
+  }
+
+  public function createContentTypesFromXml($xml){
+    foreach($xml as $books){
+      foreach($books as $book){
+        $title = $book['title'];
+        $price = $book['price'];
+        $isbn = $book['@attributes']['ISBN'];
+        $comments = $book['comments']['userComment'];
+        $comms = [];
+        if(is_array($comments)){
+          foreach($comments as $comment){
+            $comm = $comment;
+            $comms[] = $comm;
+          }
+        }else{
+          $comm = $comments;
+          $comms[] = $comm;
+        }
+        $newBook = array();
+        $newBook['type'] = 'book';
+        $newBook['title'] = $title;
+        $newBook['field_price'] = $price;
+        $newBook['field_isbn'] = $isbn;
+        $newBook['field_comments'] = $comms;
+        $bookToCreate = $this->entityTypeManager()->getStorage('node')->create($newBook);
+        $bookToCreate->save();
+      }
+    }
+  }
+
+}
